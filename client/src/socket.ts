0f8a390d3e8cfb6ce84c564bd94dfc9b66a7e788
yasho11 <yyashodeep.basnet@gmail.com>
2025-03-11 11:02:03 +0545

    Socket io setup

---
+++ client/src/socket.ts
@@ -0,0 +1,21 @@
+import { io } from "socket.io-client";
+
+const SOCKET_URL = "http://localhost:1256";
+
+export const socket = io(SOCKET_URL, {
+  autoConnect: false,
+});
+
+export const connectSocket = () => {
+  if (!socket.connected) {
+    socket.connect();
+    console.log("Socket connected");
+  }
+};
+
+export const disconnectSocket = () => {
+  if (socket.connected) {
+    socket.disconnect();
+    console.log("Socket disconnected");
+  }
+};
